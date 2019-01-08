abb567535e5e701547895da4163c1c05e49320fe
Marc MERLIN <marc_soft@merlins.org>
2019-01-07 22:20:21 -0800

    Fixes for NEOMATRIX implementation
    - re-ran as NeoMatrix/M16BY16T4 by default
    - neomatrix doesn't have custom matrix_clear and matrix_show anymore
    - re-org'ed neomatrix_config a lot
    
    TESTED=M16BY16T4

--- size.h
+++ size.h
@@ -1,5 +1,10 @@
 #ifndef size_h
 #define size_h
 // control if we decode in 32x32 or 64x64, or something else
-#define matrix_size 64
+#define matrix_size 32
+
+// If the matrix is a different size than the GIFs, set the offset for the upper left corner
+// (negative or positive is ok).
+#define OFFSETX 0
+#define OFFSETY 0
 #endif
