7efee4130757b0218e402c7563bb5a02be06bdcc
Marc MERLIN <marc_soft@merlins.org>
2019-05-09 10:07:52 -0700

    Fixed ESP8266 support and better documented NeoMatrix usage.

--- SimpleGifAnimViewer2/SimpleGifAnimViewer2.ino
+++ SimpleGifAnimViewer2/SimpleGifAnimViewer2.ino
@@ -1,5 +1,8 @@
 #define BASICSPIFFS
+// Use NeoMatrix API, even if it may use the SmartMatrix backend depending on the CPU
+#define NEOMATRIX
 #include "GifAnim_Impl.h"
+
 
 // If the matrix is a different size than the GIFs, allow panning through the GIF
 // while displaying it, or bouncing it around if it's smaller than the display
@@ -8,10 +11,16 @@ int OFFSETY = 0;
 int FACTX = 0;
 int FACTY = 0;
 
+
 const char *pathname = "/gifs64/200_circlesmoke.gif";
 
 // Setup method runs once, when the sketch starts
 void setup() {
+#ifdef ESP8266
+    // 32x32 GIFs on 24x32 display, hence offset of -4
+    OFFSETX = -4;
+    char *pathname = "/gifs/concentric_circles.gif";
+#endif
     sav_setup();
     if (sav_newgif(pathname)) delay(100000); // while 1 loop only triggers watchdog on ESP chips
 }
