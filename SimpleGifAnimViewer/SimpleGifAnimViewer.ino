4d5e5976e2418900cab8e8f5517a23eee1848d2c
Marc MERLIN <marc_soft@merlins.org>
2019-05-10 19:23:35 -0700

    Make FACTx an int, more efficient than float.

--- SimpleGifAnimViewer/SimpleGifAnimViewer.ino
+++ SimpleGifAnimViewer/SimpleGifAnimViewer.ino
@@ -6,8 +6,8 @@
 // while displaying it, or bouncing it around if it's smaller than the display
 int OFFSETX = 0;
 int OFFSETY = 0;
-float FACTX = 0;
-float FACTY = 0;
+int FACTX = 0;
+int FACTY = 0;
 
 #ifdef ESP8266
 #include <FS.h>
--- SimpleGifAnimViewer2/SimpleGifAnimViewer2.ino
+++ SimpleGifAnimViewer2/SimpleGifAnimViewer2.ino
@@ -8,8 +8,8 @@
 // while displaying it, or bouncing it around if it's smaller than the display
 int OFFSETX = 0;
 int OFFSETY = 0;
-float FACTX = 0;
-float FACTY = 0;
+int FACTX = 0;
+int FACTY = 0;
 
 
 const char *pathname = "/gifs64/200_circlesmoke.gif";
